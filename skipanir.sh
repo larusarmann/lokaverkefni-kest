00f6cca0dd3dfd9f7adee4f43ba9eaa1554b6058
larusarmann <54886138+larusarmann@users.noreply.github.com>
2019-12-02 10:53:49 +0000

    Add files via upload

---
+++ skipanir.sh
@@ -0,0 +1,128 @@
+    1  mkdir lokaverkefni
+    2  cd lokaverkefni
+    3  touch skipanir.sh
+    4  tree
+    5  sudo apt install tree
+    6  ls
+    7  lls
+    8  ll
+    9  car skipanir.sh
+   10  cat skipanir.sh
+   11  cd ..
+   12  snap "code" 
+   13  sudo apt install code
+   14  sudo apt install gnome-shell-extentions
+   15  cd lokaverkefni
+   16  mkdir {gjafadeild, leikdeild, songdeild}
+   17  tree
+   18  rm -R
+   19  rm -r
+   20  rm -l
+   21  rm -L
+   22  rm *
+   23  rm -r *
+   24  tree
+   25  touch skipanir.sh
+   26  mkdir gjafadeild,leikdeild,songdeild
+   27  tree
+   28  rm -r *
+   29  mkdir {gjafadeild,leikdeild,songdeild}
+   30  tree
+   31  touch skipanir.sh
+   32  tree
+   33  sudo groupadd -r gjafadeild
+   34  sudo groupadd -r songdeild
+   35  sudo groupadd -r leikdeild
+   36  poweroff
+   37  tree
+   38  mv lokaverkefni jolagogn
+   39  tree
+   40  cd jolagogn
+   41  mkdir sameign
+   42  tree
+   43  ls
+   44  ll
+   45  sudo groupadd -r allir
+   46  tree
+   47  cat /etc/group
+   48  treetree
+   49  tree
+   50  cat /etc/group
+   51  tree
+   52  cd ..
+   53  cd /etc/skel
+   54  mkdir vinna
+   55  sudo mkdir vinna
+   56  sudo mkdir leikir
+   57* cd 
+   58  cd /
+   59  sudo mv /home/larus/jolagogn .
+   60  ll
+   61  ll jolagogn
+   62  cd /etc/skel
+   63  ln -s /jolgogn sameign sameign
+   64  cd /
+   65  cd jolagogn
+   66  tree
+   67  cd /etc/skel
+   68  sudo ln -s /jolgogn sameign sameign
+   69  sudo ln -s /jolgogn/sameign/ sameign
+   70  sudo ln -s /jolgogn/gjafadeild/ gjafadeild
+   71  sudo ln -s /jolgogn/songdeild/ songdeild
+   72  tree
+   73  
+   74  sudo ln -s /jolgogn/leikdeild/ leikdeild
+   75  tree
+   76  cd ..
+   77  cd /
+   78  cd jolagogn
+   79  cd /etc/users
+   80  cd /etc/user
+   81  cd /etc/group
+   82  cat /etc/group
+   83  sudo useradd -G allir,gjafadeild,leikdeild,songdeild -c 'Gryla' -m gryla
+   84* sudo user
+   85  sudo useradd -G allir,gjafadeild -c 'Stekkjastaur' -m staur
+   86  sudo useradd -G allir,gjafadeild -c 'Giljagaur' -m gaur
+   87  sudo useradd -G allir,gjafadeild -c 'Stufur' -m gaur
+   88  sudo useradd -G allir,gjafadeild -c 'Stufur' -m stufur
+   89  sudo useradd -G allir,gjafadeild -c 'Thvorusleikir' -m thvari
+   90  sudo useradd -G allir,gjafadeild -c 'Pottaskefill' -m skefill
+   91  sudo useradd -G allir,gjafadeild -c 'Askasleikir' -m askur
+   92  sudo useradd -G allir,gjafadeild -c 'Hurdaskellir' -m skellir
+   93  tail /etc/group
+   94  sudo useradd -G allir,leikdeild -c 'Skyrgamur' -m gamur
+   95  sudo useradd -G allir,leikdeild -c 'Bjugnakraekir' -m bjugi
+   96  sudo useradd -G allir,leikdeild -c 'Gluggagaegir' -m gluggi
+   97  sudo useradd -G allir,songdeild -c 'Gattathefur' -m gatti
+   98  sudo useradd -G allir,songdeild -c 'Ketkrokur' -m krokur
+   99  sudo useradd -G allir,songdeild -c 'Kertasnikir' -m kerti
+  100  cat /etc/group
+  101  passwd gryla
+  102  sudo su =
+  103  sudo su -
+  104  ll
+  105  ls
+  106  ll
+  107  sudo chgrp -R gjafadeild gjafadeild
+  108  sudo chgrp -R leikdeild leikdeild
+  109  sudo chgrp -R sameign sameign 
+  110  sudo chgrp -R allir sameign
+  111  sudo chgrp -R songdeild songdeild
+  112  ll
+  113  chmod 1770 gjafadeild/
+  114  chmod 1770 leikdeild/
+  115  chmod 1770 songdeild/
+  116  cat /etc/group
+  117  cat /etc/sudoers
+  118  sudo cat /etc/sudoers
+  119  sudo su-
+  120  sudo su -
+  121  chmod 1770 sameign/
+  122  cat /etc/sudoers
+  123  sudo usermod -aG sudo gryla
+  124  cat /etc/group
+  125  history
+  126  cat history > skipanir.sh
+  127  cp history > skipanir.sh
+  128  history > skipanir.sh
